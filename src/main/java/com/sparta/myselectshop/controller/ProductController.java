8aa14fbcefd5678d3653dac4e51f9e676ca1bced
jeonjuho <wjswngh23@daum.net>
2025-02-04 11:25:54 +0900

    feat/my select shop

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,93 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import com.sparta.myselectshop.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+  private final ProductService productService;
+
+  // 관심 상품 등록하기
+  @PostMapping("/products")
+  public ProductResponseDto createProduct(
+      @RequestBody ProductRequestDto requestDto,
+      @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    // 응답 보내기
+    return productService.createProduct(requestDto, userDetails.getUser());
+  }
+
+  @PutMapping("/products/{id}")
+  public ProductResponseDto updateProduct(
+      @PathVariable("id") Long id,
+      @RequestBody ProductMypriceRequestDto requestDto
+  ) {
+    return productService.updateProduct(id, requestDto);
+  }
+
+  // 관심 상품 조회하기
+  @GetMapping("/products")
+  public Page<ProductResponseDto> getProducts(
+      @RequestParam("page") int page,
+      @RequestParam("size") int size,
+      @RequestParam("sortBy") String sortBy,
+      @RequestParam("isAsc") boolean isAsc,
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    // 응답 보내기
+    return productService.getProducts(userDetails.getUser(), page - 1, size, sortBy, isAsc);
+  }
+
+  @GetMapping("/admin/products")
+  public List<ProductResponseDto> getAllProducts() {
+    return productService.getAllProducts();
+  }
+
+  // 상품에 폴더 추가
+  @PostMapping("/products/{productId}/folder")
+  public void addFolder(
+      @PathVariable("productId") Long productId,
+      @RequestParam("folderId") Long folderId,
+      @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    productService.addFolder(productId, folderId, userDetails.getUser());
+  }
+
+  // 회원이 등록한 폴더 내 모든 상품 조회
+  @GetMapping("/folders/{folderId}/products")
+  public Page<ProductResponseDto> getProductsInFolder(
+      @PathVariable("folderId") Long folderId,
+      @RequestParam("page") int page,
+      @RequestParam("size") int size,
+      @RequestParam("sortBy") String sortBy,
+      @RequestParam("isAsc") boolean isAsc,
+      @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    return productService.getProductsInFolder(
+        folderId,
+        page-1,
+        size,
+        sortBy,
+        isAsc,
+        userDetails.getUser()
+    );
+  }
+
+}
